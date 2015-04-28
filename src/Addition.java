2b76565d56699205cb550b1029ff53a82b690f35
S199709362 <S199709362@LDHS-CY7-100551.ad.ocdsb.ca>
2015-04-28 10:48:42 -0400

    Addition function level one

---
+++ src/Addition.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author CAMERON 
+ */
+public class Addition {
+    int level;
+    
+    void setLevel(int PlaceHolder){
+        level = PlaceHolder;
+    }
+    
+    public boolean AdditionLevel1(){
+    Random random = new Random();
+    Scanner scanner = new Scanner(System.in);
+    boolean correct = false;
+    
+    int number1 = Math.abs(random.nextInt(100));
+    int number2 = Math.abs(random.nextInt(100));
+    
+    System.out.println(number1+" + "+number2+" = ");
+    int userAnswer = scanner.nextInt();
+    
+    if(userAnswer == (number1+number2)){
+        correct = true;
+    }
+    
+    System.out.println(correct);
+    return correct;
+    }
+}
