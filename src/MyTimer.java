1136ac0712c005bb6649609d2285c49e8ffc799a
Kieran Bingham <kbingham.ca@gmail.com>
2015-04-28 10:16:52 -0400

    Added timer class (beta)

---
+++ src/MyTimer.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ *
+ * @author Kieran Bingham
+ */
+public class MyTimer {
+    static int interval;
+    static Timer timer;
+    int secs = 30; 
+    boolean done = false;
+    
+    void setSecs(int input){
+        secs = input; 
+    }
+     
+    void coundtdownFrom(int input){
+//        static Timer timer;
+        boolean done = false; 
+        int delay = input*100;
+        int period = input*100;
+        timer = new Timer();
+        interval = input;
+        System.out.println(input);
+        timer.scheduleAtFixedRate(new TimerTask() {
+            public void run() {
+                System.out.println(setInterval());
+            }
+        }, delay, period);
+    }
+    
+    boolean checkFinished(){
+        return done; 
+    }
+    
+    void countdown() {
+        int delay = 1000;
+        int period = 1000;
+        timer = new Timer();
+        interval = secs;
+        System.out.println(secs);
+        timer.scheduleAtFixedRate(new TimerTask() {
+            public void run() {
+                System.out.println(setInterval());
+
+            }
+        }, delay, period);
+    }
+    
+    int setInterval() {
+        if (interval == 1) {
+            timer.cancel();
+            done = true; 
+            System.out.println("done is - "+done);
+        }
+        return --interval;
+    }
+    
+//    Timer timer = new Timer(500, new ActionListener() {
+//        public void actionPerformed(ActionEvent e) {
+//            int index = random.nextInt(colors.size());
+//            bgColor = colors.get(index);
+//            repaint();
+//        }
+//    });
+//
+//    timer.start ();
+}
