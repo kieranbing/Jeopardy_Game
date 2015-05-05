6c2d36d113b3d8b4328d4b9503c41468697ee8ee
Administrator <Administrator@LDHS-CY4-122534.ad.ocdsb.ca>
2015-05-05 10:01:10 -0400

    addition & subtraction -add400 added -subtraction class added

--- src/Subtraction.java
+++ src/Subtraction.java
@@ -4,10 +4,34 @@
  * and open the template in the editor.
  */
 
+import java.util.Random;
+
 /**
  *
  * @author Cameron Sokalski
  */
 public class Subtraction {
+     int level;
+    String question = "";
+    int number1;
+    int number2;
+    int answer;
+    boolean correct;
+
+    void setLevel(int input) {
+        level = input;
+    }
     
+    public void SubtractionLevel1(){
+        question = "";
+        correct = false;
+        
+        Random random = new Random();
+        
+        number1 = Math.abs(random.nextInt(100))+50;
+        number2 = Math.abs(random.nextInt(50));
+        
+        question = number1 + "-" + number2 + "= ?";
+        answer = number1 - number2;
+    }
 }
