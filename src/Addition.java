270235bbdb00d5aa1e0df7e6b823605aa6d2dab6
Administrator <Administrator@LDHS-CY4-122534.ad.ocdsb.ca>
2015-05-05 09:46:53 -0400

    addition -add400 added

--- src/Addition.java
+++ src/Addition.java
@@ -9,53 +9,66 @@ import java.util.Scanner;
 
 /**
  *
- * @author CAMERON 
+ * @author CAMERON
  */
 public class Addition {
+
     int level;
     String question = "";
     int number1;
     int number2;
     int answer;
     boolean correct;
-    
-    
-    void setLevel(int input){
+
+    void setLevel(int input) {
         level = input;
     }
-    
+
     void question() {
         Random random = new Random();
-        int chosen = Math.abs(random.nextInt(2)) +1;
-        switch (chosen){
+        int chosen = Math.abs(random.nextInt(2)) + 1;
+        switch (chosen) {
             case 1:
                 AdditionLevel1();
+                break;
+            case 2:
+                AdditionLevel2();
                 break;
             default:
                 System.out.println("Addition question selection error");
         }
-}
+    }
+
     //Function for level one of addition category
-    public boolean AdditionLevel1(){
-    Random random = new Random();
-    Scanner response = new Scanner(System.in);
-    correct = false;
+
+    public boolean AdditionLevel1() {
+        question = "";
+        correct = false;
+
+        Random random = new Random();
+
+        number1 = Math.abs(random.nextInt(100));
+        number2 = Math.abs(random.nextInt(100));
+
+        question = number1 + "+" + number2 + "= ?";
+        answer = number1 + number2;
+
+        return correct;
+    }
     
-    number1 = Math.abs(random.nextInt(100));
-    number2 = Math.abs(random.nextInt(100));
-    
-    question = number1+"+"+number2+"= ?";
-    answer = number1+number2;
-    
-    //System.out.println(number1+" + "+number2+" = ");
-    //int userAnswer = response.nextInt();
-    //System.out.println(correct);
-    return correct;
+    //function for level 2 of addition category
+    public void AdditionLevel2() {
+        question = "";
+        correct = false;
+        
+        Random random = new Random();
+        
+        number1 = Math.abs(random.nextInt(1000));
+        number2 = Math.abs(random.nextInt(1000));
+        
+        question = number1 + "+" + number2 +"= ?";
+        answer = number1 + number2;
+        
+        
     }
 }
-/*
-    void question() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-}
-*/
