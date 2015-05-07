853b6cc6261d598870caf603e5c821e23faa06d4
Cameron Sokalski <cameronsokalski@gmail.com>
2015-05-07 09:08:23 -0400

    Cleaning up code and adding comments

--- src/Question.java
+++ src/Question.java
@@ -1,15 +1,8 @@
-
 import javax.swing.JTextField;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
- * @author Family
+ * @author Kieran Bingham
  */
 public class Question {
     String question = "";
